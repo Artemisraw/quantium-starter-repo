9a680a35e9079fc160c350cfde0acfa7827bfa1c
Peter Ogechi <ogp1875@gmail.com>
2024-07-07 21:08:20 +0300

    final result code in pandasd.py file result of code in result.csv

--- pandasd.py
+++ pandasd.py
@@ -1,13 +1,27 @@
 import pandas as pd
+import csv
+import glob
 
-df = pd.read_csv("data/daily_sales_data_0.csv", usecols = ['product', 'quantity', 'price', 'date', 'region'])
+data_folder = "data"
+dest_file = "result.csv"
+collums = ['sales', 'date', 'region']
+csv_files = glob.glob(f"{data_folder}/*.csv")
 
-df = df.iloc[1:]
+for csv_file in csv_files:
+    df = pd.read_csv(csv_file , usecols = ['product', 'quantity', 'price', 'date', 'region'])
 
-df['price'] = df['price'].str.replace('^\$', '', regex=True)
-df['price'] = df['price'].astype(float)
-df['sales'] = df['price'] * df['quantity']
+    df = df.iloc[1:]
+    
+    df['price'] = df['price'].str.replace('^\$', '', regex=True)
+    df['price'] = df['price'].astype(float)
+    df['sales'] = df['price'] * df['quantity']
+    
+    result = df[df['product'] == "pink morsel"]
+    #final_df = result.groupby('region')
+    final_df = result[collums]
 
-result = df[df['product'] == "pink morsel"]
-#final_df = result.groupby('region')
-print(result)
+    final_df.to_csv(dest_file, index=False)
+
+
+    print(final_df)
+#print(csv_files)
